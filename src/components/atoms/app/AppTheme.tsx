a34f1d640080b49cbde1f036849875980a4036b4
suliwa <suliwayudhaa@gmail.com>
2024-12-28 20:44:07 +0700

    tetris

--- src/components/atoms/app/AppTheme.tsx
+++
@@ -1,8 +0,0 @@
-import {StylesProvider} from '@material-ui/core/styles';
-import {FC, PropsWithChildren} from 'react';
-import {useDarkMode} from '../../particles/hooks/useDarkMode';
-
-export const AppTheme: FC<PropsWithChildren> = ({children}) => {
-    useDarkMode();
-    return <StylesProvider injectFirst>{children}</StylesProvider>;
-};
